5f55d8ea94ed4c36cff6db6edc01e9d5ed4d76a6
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-03-21 22:36:10 -0300

    Primeiro BG

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,16 @@ import { CharacterSheet } from './components/CharacterSheet'
 
 const App = () => {
   return (
-    <>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        marginTop: 50,
+      }}
+    >
       <CharacterSheet />
-    </>
+    </div>
   )
 }
 
