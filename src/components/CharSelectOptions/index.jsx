b034afb4b1a3e44aeb0cf78cc764f7d9cec5f304
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-03-28 20:18:22 -0300

    Ajustando texto generico do component SelectOptions

--- src/components/CharSelectOptions/index.jsx
+++ src/components/CharSelectOptions/index.jsx
@@ -2,19 +2,14 @@ import React from 'react'
 
 const CharSelectOptions = ({ name, selectState, inChange, doc, optItem }) => {
   return (
-    <select
-              name={name}
-              id={name}
-              value={ selectState }
-              onChange={ inChange }
-            >
-              <option value="">Selecione sua origem</option>
-              {doc.map((item, i) => (
-                <option key={i} value={optItem? item[`${optItem}`] : item}>
-                  {optItem? item[`${optItem}`] : item}
-                </option>
-              ))}
-            </select>
+    <select name={name} id={name} value={selectState} onChange={inChange}>
+      <option value="">Selecione uma das opções</option>
+      {doc.map((item, i) => (
+        <option key={i} value={optItem ? item[`${optItem}`] : item}>
+          {optItem ? item[`${optItem}`] : item}
+        </option>
+      ))}
+    </select>
   )
 }
 
